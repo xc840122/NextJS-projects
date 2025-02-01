5fdd22745f4cb85a65c810979a7094e73c8df125
Chi Xu <xuchi007@hotmail.com>
2025-02-01 16:52:20 +1300

    set ConvexClientProvider

--- ticket-market-demo/app/layout.tsx
+++ ticket-market-demo/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>
-          {children}
-        </ClerkProvider>
+        <ConvexClientProvider>
+          <ClerkProvider>
+            {children}
+          </ClerkProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
