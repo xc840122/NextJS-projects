879ced285a4ad95dab997a698deb1fb8dc73d4ce
Chi Xu <xuchi007@hotmail.com>
2025-02-06 17:44:49 +1300

    fetch data, prisma

---
+++ exercises-code/app/products-db/page.tsx
@@ -0,0 +1,27 @@
+import { getProducts } from "@/prisma-db";
+
+type Product = {
+  id: number,
+  name: string,
+  price: number,
+  description: string | null;
+}
+
+export default async function ProductsDBPage() {
+  const products: Product[] = await getProducts();
+
+  return (
+    <div className="flex flex-col w-full p-2 space-y-2 h-screen bg-slate-200">
+      {products.map(product => {
+        return (
+          <div key={product.id} className=" flex flex-col rounded-lg bg-blue-800 p-2 shadow-sm text-white">
+            <h1 className="font-bold">{product.name}</h1>
+            <h3>{product.description}</h3>
+            <h3>${product.price}</h3>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
---
+++ exercises-code/prisma-db.ts
@@ -0,0 +1,65 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+const seedProducts = async () => {
+  try {
+    const count = await prisma.product.count();
+    if (count === 0) {
+      await prisma.product.createMany({
+        data: [
+          { name: "Product 1", price: 500, description: "Description 1" },
+          { name: "Product 2", price: 700, description: "Description 2" },
+          { name: "Product 3", price: 900, description: "Description 3" },
+        ],
+      });
+      console.log("Products seeded successfully!");
+    } else {
+      console.log("Products already exist, skipping seeding.");
+    }
+  } catch (error) {
+    throw new Error(`Error seeding products: ${error}`);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+await seedProducts();
+
+export async function getProducts() {
+  // await new Promise((resolve) => setTimeout(resolve, 1500));
+  return prisma.product.findMany()
+};
+
+export async function getProduct(id: number) {
+  return prisma.product.findUnique({
+    where: { id },
+  })
+};
+
+export async function addProduct(
+  name: string,
+  price: number,
+  description: string
+) {
+  return prisma.product.create({
+    data: { name, price, description },
+  });
+}
+
+export async function updateProduct(
+  id: number,
+  name: string,
+  price: number,
+  description: string
+) {
+  return prisma.product.update({
+    where: { id },
+    data: { name, price, description }
+  });
+}
+
+export async function deleteProduct(id: number) {
+  return prisma.product.delete({
+    where: { id },
+  });
+}
